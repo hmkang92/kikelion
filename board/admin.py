438a82749ee5b5a72158a30039840e2e5c25efb0
suspectt <ssoo3493@gmail.com>
2017-03-11 04:46:01 +0900

    board.admin.PostAdmin에 mark safe 적용

--- board/admin.py
+++ board/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post
 
 #admin.site.register(Post, PostAdmin)
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-	list_display = ['id', 'title', 'created_at', 'updated_at']
+	list_display = ['id', 'title', 'content_size','created_at', 'updated_at']
 
+	def content_size(self, post):
+		#return '{} length'.format(len(post.content))
+		return mark_safe('<strong>{}</string>length'.format(len(post.content)))
