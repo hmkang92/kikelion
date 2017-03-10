bc4c12d7e82b74709cc09f129fd3daf9a6fe1c15
suspectt <ssoo3493@gmail.com>
2017-03-11 03:09:01 +0900

    board.models.Post 모델 생성/마이그레이션/admin등록

--- board/models.py
+++ board/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
