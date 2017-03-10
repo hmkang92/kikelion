6661bb435ea476aa26e286f9f63ac77fd21ef256
suspectt <ssoo3493@gmail.com>
2017-03-11 04:30:22 +0900

    django-extensions 설치, admin.py 수정

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
-
+#admin.site.register(Post, PostAdmin)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['id', 'title', 'created_at', 'updated_at']
 
--- board/models.py
+++ board/models.py
@@ -1,9 +1,15 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.forms import ValidationError
+
+def content_validator(value):
+	if len(value) < 10:
+		raise ValidationError('Please enter more than 10 characters')
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
-	content = models.TextField()
+	content = models.TextField(validators=[content_validator])
+	tags = models.CharField(max_length=100, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
