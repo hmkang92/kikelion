a304a50bd9150e1ebd943447e78d0f17b81281eb
suspectt <ssoo3493@gmail.com>
2017-03-11 09:16:13 +0900

    django-debug-toolbar 구현

--- kikelion/urls.py
+++ kikelion/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^board/', include('board.urls'))
 ]
+
+if settings.DEBUG:
+	import debug_toolbar
+	urlpatterns += [
+		url(r'^__debug__/', include(debug_toolbar.urls)),
+	]
