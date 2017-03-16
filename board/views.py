b205359060ab7bf9d4ce5cc925fde64c8d3e3c44
suspectt <ssoo3493@gmail.com>
2017-03-16 21:32:12 +0900

    post_detail 추가 및 get_object_or_404 추가

--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.http import Http404
 
 def post_list(request):
 	qs = Post.objects.all()
@@ -12,3 +13,18 @@ def post_list(request):
 		'post_list': qs,
 		'q' : q,
 	})
+
+
+def post_detail(request, id):
+
+#	try:
+#		post = Post.objects.get(id=id)
+#	except Post.DoesNotExist:
+#		raise Http404
+
+	post = get_object_or_404(Post, id=id)
+
+	return render(request, 'board/post_detail.html', {
+		'post' : post,		
+	})
+
