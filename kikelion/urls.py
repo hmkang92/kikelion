2dfe5848cd0101fe7ae3f840a52389877d5fc659
suspectt <ssoo3493@gmail.com>
2017-03-18 04:06:54 +0900

    각 app마다 url namespace를 적용하고 root(람다함수)를 정의

--- kikelion/urls.py
+++ kikelion/urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.shortcuts import redirect
+from django.views.generic import RedirectView
+
+def root(request):
+	return redirect('board:post_list')
 
 urlpatterns = [
+    
+    #url(r'^$', root, name='root'),
+    url(r'^$', lambda r: redirect('board:post_list'), name='root'),
+    #url(r'^$', RedirectView.as_view(pattern_name='board:post_list')),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('accounts.urls')),
-    url(r'^board/', include('board.urls')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^board/', include('board.urls', namespace='board')),
 ]
 
 if settings.DEBUG:
