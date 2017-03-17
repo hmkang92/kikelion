6067d4405402fb1a40dd9854b150cc7c5c553eb4
suspectt <ssoo3493@gmail.com>
2017-03-18 00:53:16 +0900

    settings.AUTH_USER_MODEL 올바른 지정

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,11 @@
 # accounts/models.py
 from __future__ import unicode_literals
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-	user = models.OneToOneField(User)
+	user = models.OneToOneField(settings.AUTH_USER_MODEL)
 	student_number = models.CharField(max_length=10)
 	department = models.CharField(max_length=20)
 	phone_number = models.CharField(max_length=20)
