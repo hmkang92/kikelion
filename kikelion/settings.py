5ea83080dfb58320e44f39c1a34d5ad6ff565ae9
suspectt <ssoo3493@gmail.com>
2017-03-18 02:45:32 +0900

    project foler에 templates를 설정하고 각 앱별로 templates를 상속받아 사용하는 방식으로 변경(2 번째 방법)

--- kikelion/settings.py
+++ kikelion/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'kikelion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'kikelion', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
