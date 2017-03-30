917c83df3c7b0139267f26971faa873cdebcdb03
suspectt <ssoo3493@gmail.com>
2017-03-30 16:53:20 +0900

    filesystem static finder 설정 및 static 샘플 추가

--- kikelion/settings.py
+++ kikelion/settings.py
@@ -126,8 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'kikelion', 'static'),
+]
 
 INTERNAL_IPS = ['127.0.0.1']
+
 
 from django.contrib.messages import constants
 
