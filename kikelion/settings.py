87b87ead87f59361c2391a1538521a5fd1da2fd9
suspectt <ssoo3493@gmail.com>
2017-03-30 14:52:19 +0900

    messages framework 의 level/tags 설정 변경

--- kikelion/settings.py
+++ kikelion/settings.py
@@ -128,3 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+from django.contrib.messages import constants
+
+MESSAGE_LEVEL = constants.DEBUG
+MESSAGE_TAGS = {constants.ERROR: 'danger'}
