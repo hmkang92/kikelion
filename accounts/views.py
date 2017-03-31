95bb385855a74bc91490001fd03a9bca36c247ca
suspectt <ssoo3493@gmail.com>
2017-03-31 12:56:56 +0900

    회원가입 시에 이메일 추가 입력

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,17 @@
 # accounts/views.py
 from django.conf import settings
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
 
 def signup(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = SignupForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			return redirect(settings.LOGIN_URL)
 	else:
-		form = UserCreationForm()
+		form = SignupForm()
 
 	return render(request, 'accounts/signup_form.html', {
 		'form': form,
