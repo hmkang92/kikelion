244181e149273e2efece7dd88321e49fa4e1984e
suspectt <ssoo3493@gmail.com>
2017-03-31 13:30:34 +0900

    회원가입 시에 학번/학과/휴대폰번호 추가 입력

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,26 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class SignupForm(UserCreationForm):
+
+	phone_number = forms.CharField()
+	department = forms.CharField()
+	student_number = forms.CharField()
+
 	class Meta(UserCreationForm.Meta):
 		fields = UserCreationForm.Meta.fields + ('email',)
+
+	def save(self):
+		user = super().save()
+		profile = Profile.objects.create(
+			user = user,
+			phone_number = self.cleaned_data['phone_number'],
+			department = self.cleaned_data['department'],
+			student_number = self.cleaned_data['student_number'])
+			
+		return user
+		
+		
+
+
